7fa482f62db7ca6f4ab89f0637c4670428afd4bc
GPMrks <guilhermepereiramarques@hotmail.com>
2022-02-13 01:10:03 -0300

    Java Inferência

---
+++ DesenvolvimentoAvancadoEmJava/src/Inferencia/InferenciaExemplo.java
@@ -0,0 +1,40 @@
+package Inferencia;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class InferenciaExemplo {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(printNome("Guilherme", "Marques"));
+        System.out.println("A soma é: " + printSoma(5, 5, 10, 15.45, 3.15, 40));
+    }
+
+    private static void connectAndPrint() throws IOException {
+        var url = new URL("https://docs.oracle.com/javase/10/language/");
+        var urlConnection = url.openConnection();
+        var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+    }
+
+    public static String printNome(String nome, String sobrenome){
+        var nomeCompleto = nome + " " + sobrenome;
+        return nomeCompleto;
+    }
+
+    public static double printSoma(double... numeros){
+        double soma = 0;
+        if (numeros.length > 0){
+            for (var numero : numeros){
+                soma += numero;
+            }
+        }
+        return soma;
+    }
+
+}
