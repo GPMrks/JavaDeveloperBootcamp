356095d46e11a3b274361673cbab65b5c154e630
GPMrks <guilhermepereiramarques@hotmail.com>
2022-02-18 14:21:34 -0300

    SB Profile Configuration

---
+++ SBProfilesConfiguration/src/main/java/com/springboot/SBProfilesConfiguration/SbProfilesConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.SBProfilesConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbProfilesConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbProfilesConfigurationApplication.class, args);
+	}
+
+}
